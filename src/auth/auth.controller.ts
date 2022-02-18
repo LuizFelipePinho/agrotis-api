7eb6ac3d68585c962a0a14123baded6922a8099a
LuizFelipePinho <luizfelipesilva458@gmail.com>
2022-02-17 21:25:10 -0300

    add currentUser

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import {
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
   Request,
   UseGuards,
 } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 import { IsPublic } from './decorators/is-public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { AuthRequest } from './models/authRequest';
@@ -21,5 +24,10 @@ export class AuthController {
   @UseGuards(LocalAuthGuard)
   login(@Request() req: AuthRequest) {
     return this.authService.login(req.user);
+  }
+
+  @Get('me')
+  getMe(@CurrentUser() user: User) {
+    return user;
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,6 +16,7 @@ import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  // a ideia é pegar os dados do usuario logado e relacionar com a empresa que ele está criando, pra quando quiser deletar, atualizar ou excluir fazer isso somente na empresa que ele criou
   @Post('/create')
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
